5f3bae4fabfa9ec920008a06c797f659c5f35002
Sandi Dev <sandi.dev01@gmail.com>
2026-01-07 10:03:17 +0700

    feat: Enhance REST framework configuration with default permission and filter backends

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 # JWT Settings
